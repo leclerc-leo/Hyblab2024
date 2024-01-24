55501403e25b23054cb550afdbe83477a0022977
Quentin LAPOTRE <quentin.lapotre@etu.univ-nantes.fr>
2024-01-24 11:38:47 +0100

    Push structure basique avec lien chargement de page fonctionnel

--- quartiers-2/public/js/slide1.js
+++ quartiers-2/public/js/slide1.js
@@ -1,40 +1,36 @@
 "use strict";
 
 // async init function (because of the awaits on fetches)
-const initSlide1 = async function(){
-  // Get logo element
-  const logo = document.querySelector('#logo-hyblab');
-
-  // (Re)set initial scale of logo
-  logo.setAttribute('style', 'transform :scale(1);');
-
+const initAccueil = async function(){
   // Animate hyblab logo and make shrink on click
+    // On récupère le bouton "découvrir"
+  const decouvrir = document.querySelector('#bouton-decouvrir');
+  decouvrir.setAttribute('style', 'transform :scale(1);');
+
+  // si on clique dessus on passe à la page suivante
+  decouvrir.addEventListener('click', () => {
+      anime({
+        targets: '#bouton-decouvrir',
+        scale: 0
+      });
+    // on charge dans la page actuelle le contenu de la page suivante
+    // en utilisant la méthode fetch
+    fetch('/quartiers-2/stories.html')
+      .then(res => res.text())
+      .then(html => {
+        // on remplace le contenu de la page actuelle par le contenu de la page suivante
+        document.querySelector('body').innerHTML = html;
+        // on lance l'animation de la page suivante
+        initSlide2();
+      });
+    });
+
   anime({
-    targets: '#logo-hyblab',
-    scale: 1.2,
+    targets: '#bouton-decouvrir',
+    scale: 1.1,
     easing: 'easeInOutQuad',
     direction: 'alternate',
     loop: true
   });
 
-  // Add click listener
-  logo.addEventListener('click', () => {
-    anime({
-        targets: '#logo-hyblab',
-        scale: 0
-      });
-    swiper.slideNext()
-  });
-
-  // Retrieve the partner's topic from our API
-  let response = await fetch('api/topic');
-  const data1 = await response.json();
-
-  // Get some dummy data
-  response = await fetch('data/dummy.json');
-  const data2 = await response.json();
-
-  // Update the DOM to insert topic and data
-  const footer = document.querySelector('footer p');
-  footer.textContent = `Our topic is "${data1.topic}" and here is "${data2.message}" retrieved on the server.`;
 };
