24272f52985fdc69f7542d33e5015db8083b11ce
Moncef <moncef.bouramoul@etu.univ-nantes.fr>
2024-01-25 17:34:18 +0100

    [EVO] correction des problèmes lié à la base de données

--- beaujoire-2/public/data/dbinit.js
+++ beaujoire-2/public/data/dbinit.js
@@ -1,7 +1,7 @@
 const util = require('util');
 const sqlite3 = require('sqlite3');
 const path = require('path');
-const dbname = path.join(__dirname, 'beaujoire-2');
+const dbname = path.join(__dirname, 'beaujoire-2.db');
 
 // Ouverture de la base de données
 let db = new sqlite3.Database(dbname, err => {
@@ -10,29 +10,29 @@ let db = new sqlite3.Database(dbname, err => {
 });
 
 db.serialize(() => {
-    db.run(`
+    db.exec(`
         DROP TABLE IF EXISTS Nationalités;
         CREATE TABLE Nationalités(
-            id TINYINT PRIMARY KEY,
+            id INTEGER PRIMARY KEY autoincrement,
             nationalité VARCHAR(50)
         );
 
         DROP TABLE IF EXISTS Postes;
         CREATE TABLE Postes(
-            id TINYINT PRIMARY KEY,
+            id INTEGER PRIMARY KEY autoincrement,
             poste VARCHAR(20)
         );
 
         DROP TABLE IF EXISTS Joueurs;
         CREATE TABLE Joueurs(
-            id INT PRIMARY KEY,
+            id INTEGER PRIMARY KEY autoincrement,
             nom VARCHAR(50),
             prenom VARCHAR(50),
             age TINYINT,
             naissance DATE,
-            nationalité1 TINYINT FOREIGN KEY REFERENCES Nationalités(id),
-            nationalité2 TINYINT FOREIGN KEY REFERENCES Nationalités(id),
-            poste TINYINT FOREIGN KEY REFERENCES Postes(id),
+            nationalité1 TINYINT,
+            nationalité2 TINYINT,
+            poste TINYINT,
             AnnéeDébutJunior SMALLINT,
             AnnéeFinJunior SMALLINT,
             AnnéeDébutSenior SMALLINT,
@@ -41,66 +41,78 @@ db.serialize(() => {
             buts SMALLINT,
             photo BLOB,
             citation VARCHAR(255),
-            biographie TEXT
+            biographie TEXT,
+            FOREIGN KEY(nationalité1) REFERENCES Nationalités(id),
+            FOREIGN KEY(nationalité2) REFERENCES Nationalités(id),
+            FOREIGN KEY(poste) REFERENCES Postes(id)
         );
 
         DROP TABLE IF EXISTS Articles;
         CREATE TABLE Articles(
-            id INT PRIMARY KEY,
+            id INTEGER PRIMARY KEY autoincrement,
             article BLOB
         );
 
         DROP TABLE IF EXISTS JoueursArticles;
         CREATE TABLE JoueursArticles(
-            id_joueur INT FOREIGN KEY REFERENCES Joueurs(id),
-            id_article INT FOREIGN KEY REFERENCES Articles(id),
-            PRIMARY KEY (id_joueur, id_article)
+            id_joueur INT,
+            id_article INT,
+            PRIMARY KEY (id_joueur, id_article),
+            FOREIGN KEY (id_joueur) REFERENCES Joueurs(id),
+            FOREIGN KEY (id_article ) REFERENCES Articles(id)
         );
 
         DROP TABLE IF EXISTS Photos;
         CREATE TABLE Photos(
-            id INT PRIMARY KEY,
+            id INTEGER PRIMARY KEY autoincrement,
             photo BLOB
         );
 
         DROP TABLE IF EXISTS JoueursPhotos;
         CREATE TABLE JoueursPhotos(
-            id_joueur INT FOREIGN KEY REFERENCES Joueurs(id),
-            id_photo INT FOREIGN KEY REFERENCES Photos(id),
-            PRIMARY KEY (id_joueur, id_photo)
+            id_joueur INT,
+            id_photo INT,
+            PRIMARY KEY (id_joueur, id_photo),
+            FOREIGN KEY (id_joueur) REFERENCES Joueurs(id),
+            FOREIGN KEY (id_photo) REFERENCES Photos(id)
         );
 
         DROP TABLE IF EXISTS Vidéos;
         CREATE TABLE Vidéos(
-            id INT PRIMARY KEY,
+            id INTEGER PRIMARY KEY autoincrement,
             vidéo BLOB
         );
 
         DROP TABLE IF EXISTS JoueursVidéos;
         CREATE TABLE JoueursVidéos(
-            id_joueur INT FOREIGN KEY REFERENCES Joueurs(id),
-            id_vidéo INT FOREIGN KEY REFERENCES Vidéos(id),
-            PRIMARY KEY (id_joueur, id_vidéo)
+            id_joueur INT,
+            id_vidéo INT,
+            PRIMARY KEY (id_joueur, id_vidéo),  
+            FOREIGN KEY (id_joueur) REFERENCES Joueurs(id),
+            FOREIGN KEY (id_vidéo) REFERENCES Vidéos(id)
         );
-    `)
-    .run('INSERT INTO Postes (poste) VALUES (?)', [
-        'Gardien',
-        'Arrière latéral droit',
-        'Arriere latéral gauche',
-        'Défenseur central 1',
-        'Défenseur central 2',
-        'Milieu défensif',
-        'Milieu gauche',
-        'Milieu offensif',
-        'Attaquant 1',
-        'Milieu droit',
-        'Attaquant 2'
-    ])
-    .all('SELECT * FROM Postes', (err, rows) => {
+    `);
+    db.run(`
+        INSERT INTO Postes(poste) VALUES 
+            ('Gardien'),
+            ('Arrière latéral droit'),
+            ('Arriere latéral gauche'),
+            ('Défenseur central 1'),
+            ('Défenseur central 2'),
+            ('Milieu défensif'),
+            ('Milieu gauche'),
+            ('Milieu offensif'),
+            ('Attaquant 1'),
+            ('Milieu droit'),
+            ('Attaquant 2'),
+            ('Sélectionneur')
+    `);
+    db.all('SELECT * FROM Postes', (err, rows) => {
         if (err) console.error(err.message);
         else console.log('Résultat de la sélection : ', rows)
-    })
+    });
 })
 
+db.close();
 
 module.exports = db;
