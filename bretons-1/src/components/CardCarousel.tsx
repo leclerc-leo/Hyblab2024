c8550081eb8d54961a58648b4e768aa470302028
Nino <nino.auriede@etu.univ-nantes.fr>
2024-01-26 13:58:58 +0100

    ajout image sur la page d'accueil

--- bretons-1/src/components/CardCarousel.tsx
+++ bretons-1/src/components/CardCarousel.tsx
@@ -1,21 +1,13 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import ImageGenerique from '../assets/sportif_velo.jpeg';
 
 function KitchenSinkExample() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />{/* Ici une image lié à l'athlète sera mis */}
+      <Card.Img variant="top" src={ImageGenerique}/>{/* Ici une image lié à l'athlète sera mis */}
       <Card.Body>
         <Card.Title>Card Title</Card.Title> {/* Titre de a vidéo*/}
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>
   );
--- bretons-1/src/components/Navbar.tsx
+++ bretons-1/src/components/Navbar.tsx
@@ -28,13 +28,11 @@ function OffcanvasExample() {
                             <Offcanvas.Body>
                                 <Nav className="justify-content-end flex-grow-1 pe-3">
                                     <Nav.Link href="#action1">Home</Nav.Link>
-                                    <Nav.Link href="#action2">Link</Nav.Link>
                                     <NavDropdown
                                         title="Dropdown"
                                         id={`offcanvasNavbarDropdown-expand-${expand}`}
                                     >
-                                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action4">
+                                        <NavDropdown.Item href="#action2">
                                             Another action
                                         </NavDropdown.Item>
                                         <NavDropdown.Divider />
