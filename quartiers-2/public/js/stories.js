0920af26c97cc835ccf3c0dd469382c343998277
Quentin LAPOTRE <quentin.lapotre@etu.univ-nantes.fr>
2024-01-27 17:14:33 +0100

    Swiper fonctionnel avec seulement les boutons

--- quartiers-2/public/js/stories.js
+++ quartiers-2/public/js/stories.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Just animate the logo
-  const homeStories = function () {
+const homeStories = function () {
     // on cache le slider au chargement de la page
     document.querySelector('#mySwiper').style.display = 'none';
 
@@ -12,13 +12,29 @@
         el: ".swiper-pagination",
         clickable: true,
       },
-      allowTouchMove: true, // a mettre en false quand on aura fini de coder pour eviter de swiper
-      effects: 'cards',
+      autoHeight: true,
+      allowTouchMove: false, // a mettre en false quand on aura fini de coder pour eviter de swiper
       loop: true // pour swiper en boucle
     });
 
     swiper.on("slideChange", function () {
-      switch (swiper.activeIndex) {
+      // on récupère les boutons previous et next
+      let next = document.querySelectorAll('.next');
+      let previous = document.querySelectorAll('.previous');
+
+      next.forEach(button => {
+        button.addEventListener('click', () => {
+          swiper.slideNext();
+          document.querySelector('.content').scrollIntoView();
+        });
+      })
+      previous.forEach(button => {
+        button.addEventListener('click', () => {
+          swiper.slidePrev();
+          document.querySelector('.content').scrollIntoView();
+        });
+      })
+      switch (swiper.realIndex) {
         case 0:
           console.log('slide 1');
           break;
@@ -35,7 +51,7 @@
     let stories = document.querySelectorAll(".story");
     let swiper_slides = document.querySelectorAll(".swiper-slide");
 
-    // Pour chaque story on va chercher le contenu de la slide correspondante et ajouter un event listener
+    // Pour chaque story, on va chercher le contenu de la slide correspondante et ajouter un event listener
     for (let i = 1; i <= stories.length; i++) {
       let slideIndex = i - 1;
 
@@ -53,9 +69,41 @@
       // On ajoute les event listeners sur les stories pour afficher le slider
       story.addEventListener('click', () => {
         // on met la bonne slide active
-        swiper.slideTo(slideIndex, 0);
+        swiper.slideTo(swiper.realIndex, 0);
+
+        // on cache les stories
+        stories.forEach(story => {
+          story.style.display = 'none';
+        });
+
+
         // on affiche le swiper
         document.querySelector('#mySwiper').style.display = 'block';
+        document.querySelector('.content').scrollIntoView();
+
       });
     }
+
+    // on récupère la fleche de retour
+    let retour = document.querySelector('#back_button');
+    retour.addEventListener('click', () => {
+      // si le swiper n'est pas affiché alors on retourne à l'accueil
+      if (document.querySelector('#mySwiper').style.display === 'none') {
+        fetch('/quartiers-2/index.html')
+          .then(res => res.text())
+          .then(html => {
+            document.querySelector('body').innerHTML = html;
+            // on récupère la motion et on l'enlève
+            document.querySelector('#motion').style.display = 'none';
+            // on init l'accueil
+            initAccueil();
+          });
+      }
+      // on reaffiche les stories
+      stories.forEach(story => {
+        story.style.display = 'block';
+      });
+      document.querySelector('#mySwiper').style.display = 'none';
+      // on remet l'utilisateur en haut de la page
+    });
   };
