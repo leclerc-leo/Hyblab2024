592369f9b31ba84e0010f893fd6201f037c5571f
2Pfr <firefreeze@outlook.fr>
2024-01-28 11:15:52 +0100

    essai ratio app

--- bretons-1/src/App.tsx
+++ bretons-1/src/App.tsx
@@ -2,24 +2,29 @@ import ControlledCarousel from './components/carousel';
 import './App.css';
 import OffcanvasExample from './components/Navbar'
 import VideoList from './components/VideoList'
+import Ratio from 'react-bootstrap/Ratio';
+
 function App() {
   return (
-    <div className="container">
-      <div className="head">
-        <OffcanvasExample />
-      </div>
-      <div className='main_app'>
-        <div className="app">
-          <div className="Carousel">
-            <ControlledCarousel />
+    <div style={{ width: 480, height: 'auto'}}>
+      <Ratio aspectRatio = "9x16">
+      <div className="container">
+        <div className="head">
+          <OffcanvasExample />
+        </div>
+        <div className='main_app'>
+          <div className="app">
+            <div className="Carousel">
+              <ControlledCarousel />
+            </div>
           </div>
         </div>
+        <div className="main_video">
+          <VideoList />
+        </div>
       </div>
-      <div className="main_video">
-        <VideoList />
-      </div>
+      </Ratio>
     </div>
-
   );
 }
 
