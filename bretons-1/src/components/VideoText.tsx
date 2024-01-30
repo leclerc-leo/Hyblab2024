d5ea0c001caf86527404ffd2912a32978c664f94
Arthur <arthur.courtrai@etu.univ-nantes.fr>
2024-01-30 09:07:25 +0100

    Composant qui affiche la description de la video

--- bretons-1/src/components/VideoText.tsx
+++ bretons-1/src/components/VideoText.tsx
@@ -14,4 +14,19 @@ interface VideoTextOverlayProps {
   video: Video;
 }
 
-
+const VideoTextOverlay: React.FC<VideoTextOverlayProps> = ({ video }) => {
+    return (
+      <div className="video-container">
+        <video src={video.thumbnail} controls poster={video.thumbnail}> {}
+          Your browser does not support the video tag.
+        </video>
+        <div className="overlay-text">
+          <h1>{video.title}</h1>
+          <h2>{video.subtitle}</h2>
+          <p>{video.description}</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default VideoTextOverlay;
