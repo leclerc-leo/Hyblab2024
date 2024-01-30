d33f8cbbf9c987cdba82d04bcbd0fc4e6a01726b
Vigilioon <vigilioon@grandparisrp.fr>
2024-01-30 15:35:00 +0100

    Ajout image dans l'effect

--- bretons-1/src/remotion/Frame4.tsx
+++ bretons-1/src/remotion/Frame4.tsx
@@ -3,8 +3,8 @@ import './Frame4.css';
 
 export const Frame4: React.FC<{ text: string }> = ({ text }) => {
   const [_, setIsLoading] = useState(true);
-  console.log(text);
   useEffect(() => {
+      console.log(text);
       setIsLoading(false);
       let delay = 20,
       delay_start = 0,
@@ -12,20 +12,41 @@ export const Frame4: React.FC<{ text: string }> = ({ text }) => {
       letters;
   
       let elem = document.getElementById("animatedtext")
-      console.log(elem);
       contents = elem!.textContent!.trim();
       elem!.textContent = "";
       letters = contents.split("");
       elem!.style.visibility = 'visible';
-  
+      let stopImage:string = "";
+      let start = false;
       letters.forEach(function (letter, index_1) {
-        setTimeout(function () {
-          elem!.textContent += letter;
-        }, delay_start + delay * index_1);
+        if(letter=="_") {
+          if(stopImage != "") {
+            var image = document.createElement("img");
+            image.src = "/bretons-1/img/"+stopImage+".png";
+            image.style.width = "50px";
+            setTimeout(function () {
+              elem?.appendChild(image);
+            }, delay_start + delay * index_1);
+            stopImage = "";
+            start = false;
+          } else {
+            start = true;
+          }
+        } else {
+          if(start) {
+            stopImage += letter;
+          } else {
+            setTimeout(function () {
+              elem!.append(letter);
+            }, delay_start + delay * index_1);
+          }
+        }
       });    
       delay_start += delay * letters.length;
     }, []);
     return (
-      <p id='animatedtext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <>
+        <p id='animatedtext'>Lorem ipsum dolor sit amet, _GoldMedal_ consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </>
     );
 };
