1ddd67c73e59fbe337fe3342be5b956d722d1c5a
Nino <nino.auriede@etu.univ-nantes.fr>
2024-01-31 16:51:05 +0100

    Modificaton du menu Burger et de la mise en page de l'accueil

--- bretons-1/src/components/Navbar.tsx
+++ bretons-1/src/components/Navbar.tsx
@@ -1,34 +1,16 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import BurgerMenu from './BurgerMenu';
 import './Navbar.css';
 
 function OffcanvasExample() {
     return (
         <>
             {['xxxl'].map((expand, index) => (
-                <Navbar key={index.toString()} expand={expand} className="bg-body-tertiary mb-3">
+                <Navbar fixed="top" key={index.toString()} expand={expand} className="bg-body-tertiary" data-bs-theme="light">
                     <Container fluid>
                         <Navbar.Brand><img src="/bretons-1/img/Logo_Treact.png" alt='Logo' /></Navbar.Brand>
-                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                        <Navbar.Offcanvas
-                            id={`offcanvasNavbar-expand-${expand}`}
-                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                            placement="end"
-                        >
-                            <Offcanvas.Header closeButton>
-                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                                    Menu
-                                </Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body>
-                                <Nav className="justify-content-end flex-grow-1 pe-3">
-                                    <Nav.Link href="/bretons-1">Home</Nav.Link>
-                                    <Nav.Link href="/bretons-1/MySave">My Save</Nav.Link>
-                                </Nav>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
+                        <BurgerMenu></BurgerMenu>
                     </Container>
                 </Navbar>
             ))}
