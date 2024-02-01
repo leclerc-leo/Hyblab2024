4912d5739db6c1e8f5e94e829f48e70a442adfc1
leclerc-leo <leo@leclerc.dev>
2024-02-01 11:13:57 +0100

    ajout le support pour différente pages

--- gymnastique-2/public/js/util.js
+++ gymnastique-2/public/js/util.js
@@ -62,3 +62,24 @@ const move_background = (x, b = false) => {
 
     background.style.left = `${left}px`;
 }
+
+const handle_orientation = (event) => {    
+    document.querySelectorAll('.background-move-high').forEach( element => {
+        const maxY = 30;
+        const y = Math.min(event.gamma, 90) + 90;
+        const left = `${(maxY * y) / 180 - maxY}px`;
+        element.style.left = left;
+    });
+    document.querySelectorAll('.background-move-med').forEach( element => {
+        const maxY = 20;
+        const y = Math.min(event.gamma, 90) + 90;
+        const left = `${(maxY * y) / 180 - maxY}px`;
+        element.style.left = left;
+    });
+    document.querySelectorAll('.background-move-low').forEach( element => {
+        const maxY = 10;
+        const y = Math.min(event.gamma, 90) + 90;
+        const left = `${(maxY * y) / 180 - maxY}px`;
+        element.style.left = left;
+    });
+}
