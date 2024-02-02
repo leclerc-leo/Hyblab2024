060cfbe23ff418416475f7d340a65768a48006cb
arkhuss <arkhas_bou@outlook.fr>
2024-02-02 12:06:31 +0100

    merge

--- gymnastique-1/public/js/slide1.js
+++ gymnastique-1/public/js/slide1.js
@@ -1,43 +1,47 @@
 "use strict";
-
-// async init function (because of the awaits on fetches)
-const initSlide2 = async function () {
-  // Get logo element
-  const logo = document.querySelector("#logo-hyblab");
-
-  // (Re)set initial scale of logo
-  logo.setAttribute("style", "transform :scale(1);");
-
-  // Animate hyblab logo and make shrink on click
-  anime({
-    targets: "#logo-hyblab",
-    scale: 1.2,
-    easing: "easeInOutQuad",
-    direction: "alternate",
-    loop: true,
-  });
-
-  document.getElementById("logo-hyblab").addEventListener("click", function () {
-    var popup = document.getElementById("popup");
-    popup.style.display = "block";
-    setTimeout(function () {
-      popup.classList.add("show");
-    }, 20);
-  });
-
-  document.querySelector(".close").addEventListener("click", function () {
-    var popup = document.getElementById("popup");
-    popup.classList.remove("show");
-    setTimeout(function () {
-      popup.style.display = "none";
-    }, 300);
-  });
-
-  // Retrieve the partner's topic from our API
-  let response = await fetch("api/topic");
-  const data1 = await response.json();
-
-  // Get some dummy data
-  response = await fetch("data/dummy.json");
-  const data2 = await response.json();
+const showPopup = (name) => {
+  const popup = document.getElementById(name);
+  popup.style.display = "block";
+  setTimeout(() => {
+    popup.classList.add("show");
+  }, 20);
 };
+
+const closePopup = (name) => {
+  const popup = document.getElementById(name);
+  popup.classList.remove("show");
+  setTimeout(() => {
+    popup.style.display = "none";
+  }, 20);
+};
+
+const clickOutsidePopup = (name, event) => {
+  const popup = document.getElementById(name);
+  if (!popup.contains(event.target) && popup.classList.contains("show")) {
+    closePopup(name);
+  }
+};
+
+const initSlide2 = async function () {
+  document.getElementById("algerie").addEventListener("click", (evt) =>{showPopup("popup_algerie");});
+
+  try {
+    const response = await fetch("data/obj.json");
+    const data = await response.json();
+
+    const title = document.querySelector("#title-obj");
+    const text = document.querySelector("#text-obj");
+    const img = document.querySelector("#img-obj");
+
+    title.textContent = data.title || " PAS DE TITRE ";
+    text.textContent = data.text || " PAS DE TEXTE ";
+
+    img.setAttribute("src", data.picture);
+
+  } catch (error) {
+    console.error("ERREUR JSON :", error);
+  }
+
+  document.querySelector("#close").addEventListener("click", (evt) =>{closePopup("popup_algerie")});
+  document.addEventListener("click",(evt) =>{ clickOutsidePopup("popup_algerie", evt)});
+}; 
