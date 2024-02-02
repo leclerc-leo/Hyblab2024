c81125f350917e67b7e5dbe06915d5209890b063
Heloïse DELASALLE-SEMBLAT <heloise.semblat@hotmail.fr>
2024-02-02 16:51:11 +0100

    Changes on selection of the writers' choice page

---
+++ beaujoire-1/public/js/selection.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
