a88f5cafeb6752a8fdb0a2db7d2ff8471e2f9855
leclerc-leo <leo@leclerc.dev>
2024-02-02 10:53:42 +0100

    fix des animations pour l'ouverture des pages de catégories

--- gymnastique-2/public/js/index.js
+++ gymnastique-2/public/js/index.js
@@ -67,7 +67,8 @@ categories_buttons.forEach( button => {
         block.style.width = '100%';
         block.style.height = '100vh';
         block.style.bottom = '0';
-        block.style.left = '0';
+        if (block.style.left != '') block.style.left = '0';
+        if (block.style.right != '') block.style.right = '0';
         block.style.overflow = 'auto';
         block.style.borderRadius = '0';
         block.style.opacity = '1';
