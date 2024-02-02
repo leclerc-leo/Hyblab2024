e5ce54c3f7341e4fafd223beea079cfbf910ac19
Arthur <arthur.courtrai@etu.univ-nantes.fr>
2024-02-02 08:42:11 +0100

    modif html

--- bretons-1/src/remotion/Frame2.tsx
+++ bretons-1/src/remotion/Frame2.tsx
@@ -6,6 +6,18 @@ export const Frame2: React.FC = () => {
         <div className="frameContainer">
             <div className="titre">Julie Bresset, arrive 1ère avec 1min d’avance !</div>
             <div className="rectangleYellow">
+                <svg
+                    width="76"
+                    height="76"
+                    viewBox="0 0 76 76"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="leftSVG"
+                >
+                    <path d="M37.6408 75.2815C58.4292 75.2815 75.2815 58.4292 75.2815 37.6408C75.2815 16.8524 58.4292 0 37.6408 0C16.8524 0 0 16.8524 0 37.6408C0 58.4292 16.8524 75.2815 37.6408 75.2815Z" fill="#25283D"/>
+                    <path d="M37.6409 67.6505C54.215 67.6505 67.6509 54.2146 67.6509 37.6405C67.6509 21.0665 54.215 7.63056 37.6409 7.63056C21.0669 7.63056 7.63098 21.0665 7.63098 37.6405C7.63098 54.2146 21.0669 67.6505 37.6409 67.6505Z" fill="#FACD5F"/>
+                    <path d="M27.4005 54.0508V48.9183H35.5434V25.7729L27.4005 31.5469V25.4274L35.7408 19.5053H41.4655V48.9183H48.1278V54.0508H27.4005Z" fill="#25283D"/>
+                </svg>
                 <div className="info">
                     <span className="name">Julie Bresset</span>
                     <span className="time">Tps : 1 h 30 min 52 s</span>
