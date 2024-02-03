ed76366537cfc2eb78cc1ad03ee7ce17afa8bc63
HadilHelali <helalihadilprof@gmail.com>
2024-02-03 21:52:57 +0100

    Adding the pop-up and archive button enable function

--- beaujoire-2/public/js/field.js
+++ beaujoire-2/public/js/field.js
@@ -1,5 +1,45 @@
 "use strict";
 
+globalThis.nbreVotes = 12 ; 
+
+/************* Archives *************/
+function checkVotes() {
+  var archivesButton = document.getElementById('archives');
+  if (globalThis.nbreVotes === 12) {
+    archivesButton.classList.remove("disabled");
+    archivesButton.classList.add("enabled");
+    archivesButton.href = "/beaujoire-2/archives";
+  } else {
+    archivesButton.classList.remove("enabled");
+    archivesButton.classList.add("disabled");
+    archivesButton.href = "javascript: void(0)";
+  }
+}
+/************* Archives *************/
+
+/************* Pop up  **************/
+
+if (globalThis.nbreVotes === 12) {
+
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+          document.getElementById('container').style.opacity = 0.7 ;
+      },
+      1000
+  )
+});
+
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+  document.getElementById('container').style.opacity = 1 ;
+}); }
+
+/************* Pop up  **************/
+
+
+
 // Wait for the content to preload and display 1st slide
 // Here we simulate a loading time of one second
 setTimeout(() => { 
@@ -14,3 +54,4 @@ setTimeout(() => {
   });
 }, 1000);
 
+checkVotes();
