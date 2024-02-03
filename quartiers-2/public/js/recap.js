71d3673366b47d3cd3675e7cb55fbcb2f898e9b0
Quentin LAPOTRE <quentin.lapotre@etu.univ-nantes.fr>
2024-02-03 16:42:08 +0100

    Recap Map

--- quartiers-2/public/js/recap.js
+++ quartiers-2/public/js/recap.js
@@ -1,97 +1,116 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-// to return to a new page
-function redirectToPage(url) {
-  window.location.href = url;
-};
-
 //show the map
-var map = L.map('map').setView([48.10975380676096, -1.6629224283773485], 14);
+const recapPage = function () {
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 20,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-// define the Icon
-var LeafIcon = L.Icon.extend({
-  options: {
-      iconSize:     [38, 30],
-      iconAnchor:   [0, 0], //offset of icon
-      popupAnchor:  [0, 0]  //offset of popup
+  function getInitialZoomLevel() {
+    var width = window.innerWidth;
+    if (width <= 480) {
+        return 13; // Zoom level for mobile devices
+    } else if (width <= 768) {
+        return 14; // Zoom level for tablets
+    } else {
+        return 15; // Zoom level for desktop
+    }
   }
-});
 
-//define the images for different icons
-var claude = new LeafIcon({iconUrl: 'https://i.ibb.co/Fq8wjty/claude.png'}),
-    marlene = new LeafIcon({iconUrl: 'https://i.ibb.co/grH7wvd/marlene.png'}),
-    marie = new LeafIcon({iconUrl: 'https://i.ibb.co/9w2PYTB/marie.png'}),
-    frederic = new LeafIcon({iconUrl: 'https://i.ibb.co/n8FW0K5/frederic.png'}),
-    joachim = new LeafIcon({iconUrl: 'https://i.ibb.co/7KB7V4G/joachim.png'}),
-    nicolas = new LeafIcon({iconUrl: 'https://i.ibb.co/RTFC5SZ/nicolas.png'}),
-    christophe = new LeafIcon({iconUrl: 'https://i.ibb.co/zb3pV07/christophe.png'});
+  function getIconSize() {
+    var width = window.innerWidth;
+    if (width <= 480) {
+        return 50; // Icon size for mobile devices
+    } else if (width <= 768) {
+        return 60; // Icon size for tablets
+    } else {
+        return 70; // Icon size for desktop
+    }
+  }
 
+  // Use the function to set the initial zoom level
+  var map = L.map('map').setView([48.10975380676096, -1.6629224283773485], getInitialZoomLevel());
 
-//create the marker
-L.marker([48.11154453515815, -1.6660518805101123], {icon: claude}).addTo(map).bindPopup("<h3>Claude</h3><img src='https://i.ibb.co/Fq8wjty/claude.png' alt='Claude Image'>" +
-"<button id='claude-button' class='popup-button'>Découvrez Son Histoire</button>");
-L.marker([48.10628791195435, -1.6660237580689445], {icon: marlene}).addTo(map).bindPopup("<h3>Marlène</h3><img src='https://i.ibb.co/grH7wvd/marlene.png' alt='Marlène Image'>" +
-"<button id='marlene-button' class='popup-button'>Découvrez Son Histoire</button>");
-L.marker([48.112826336880836, -1.664164031084723], {icon: marie}).addTo(map).bindPopup("<h3>Marie</h3><img src='https://i.ibb.co/9w2PYTB/marie.png' alt='Marie Image'>" +
-"<button id='marie-button' class='popup-button'>Découvrez Son Histoire</button>");
-L.marker([48.10888969480733, -1.6716351464288817], {icon: frederic}).addTo(map).bindPopup("<h3>Frédéric</h3><img src='https://i.ibb.co/n8FW0K5/frederic.png' alt='Frédéric Image'>" +
-"<button id='frederic-button' class='popup-button'>Découvrez Son Histoire</button>");
-L.marker([48.10717800781931, -1.6726050662616248 ], {icon: joachim}).addTo(map).bindPopup("<h3>Joachim</h3><img src='https://i.ibb.co/7KB7V4G/joachim.png' alt='Joachim Image'>" +
-"<button id='joachim-button' class='popup-button'>Découvrez Son Histoire</button>");
-L.marker([48.10732876940272, -1.6711969681481214 ], {icon: nicolas}).addTo(map).bindPopup("<h3>Nicolas</h3><img src='https://i.ibb.co/RTFC5SZ/nicolas.png' alt='Nicolas Image'>" +
-"<button id='nicolas-button' class='popup-button'>Découvrez Son Histoire</button>");
-L.marker([48.10855480875185, -1.6518949960950546 ], {icon: christophe}).addTo(map).bindPopup("<h3>Christophe</h3><img src='https://i.ibb.co/zb3pV07/christophe.png' alt='Christophe Image'>" +
-"<button id='christophe-button' class='popup-button'>Découvrez Son Histoire</button>");
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 20,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
 
-document.addEventListener('DOMContentLoaded', function() {
-
-  console.log("DOMContentLoaded event fired");
-
-
-
-  // get all buttons classified as 'popup-button' 
-  var popupButtons = document.querySelectorAll('.popup-button');
-
-  console.log("Buttons with '.popup-button' class:", popupButtons);
-
-  // add listener to each button
-  popupButtons.forEach(function(button) {
-      button.addEventListener('click', function() {
-          // add logic to button according to the unique id
-          var buttonId = button.id;
-          switch (buttonId) {
-              case 'claude-button':
-                  redirectToPage('recap.html');
-                  break;
-              case 'marlene-button':
-                  redirectToPage('recap.html');
-                  break;
-              case 'marie-button':
-                  redirectToPage('recap.html');
-                  break;
-              case 'frederic-button':
-                  redirectToPage('recap.html');
-                  break;
-              case 'joachim-button':
-                  redirectToPage('recap.html');
-                  break;
-              case 'nicolas-button':
-                  redirectToPage('recap.html');
-                  break;
-              case 'christophe-button':
-                  redirectToPage('recap.html');
-                  break;
-                  
-              // 添加其他按钮的逻辑
-          }
-      });
+  // define the Icon
+  var LeafIcon = L.Icon.extend({
+    options: {
+        iconSize:     [getIconSize(), getIconSize()], // size of the icon
+        iconAnchor:   [0, 0], //offset of icon
+        popupAnchor:  [0, 0]  //offset of popup
+    }
   });
-});
+
+  //define the images for different icons
+  var claude = new LeafIcon({iconUrl: 'https://i.ibb.co/Fq8wjty/claude.png'}),
+      marlene = new LeafIcon({iconUrl: 'https://i.ibb.co/grH7wvd/marlene.png'}),
+      marie = new LeafIcon({iconUrl: 'https://i.ibb.co/9w2PYTB/marie.png'}),
+      frederic = new LeafIcon({iconUrl: 'https://i.ibb.co/n8FW0K5/frederic.png'}),
+      joachim = new LeafIcon({iconUrl: 'https://i.ibb.co/7KB7V4G/joachim.png'}),
+      nicolas = new LeafIcon({iconUrl: 'https://i.ibb.co/RTFC5SZ/nicolas.png'}),
+      christophe = new LeafIcon({iconUrl: 'https://i.ibb.co/zb3pV07/christophe.png'});
+
+
+  //create the marker
+  L.marker([48.11154453515815, -1.6660518805101123], {icon: claude}).addTo(map).bindPopup(
+    "<div><h3>Claude</h3><img src='https://i.ibb.co/Fq8wjty/claude.png' alt='Claude Image'></div>" +
+    "<div>" +
+      "<h3>Adresse Adresse Adresse Adresse Adresse Adresse</h3>" +
+      "<button id='claude-button' class='popup-button'><a target='_blank' href='https://www.google.com/maps/place/local+libertaire+%22La+Commune%22/@48.1115294,-1.6661136,17z/data=!3m1!4b1!4m6!3m5!1s0x480edf5c8f478afd:0x4b17773367e94451!8m2!3d48.1115294!4d-1.6661136!16s%2Fg%2F11jm5lp9v7?hl=fr&entry=ttu\'>Y aller</a></button>" +
+      "<button id='claude-button' class='popup-button'>Découvrez Son Histoire</button>" +
+    "</div>"
+  );
+
+  L.marker([48.10628791195435, -1.6660237580689445], {icon: marlene}).addTo(map).bindPopup(
+    "<div><h3>Marlène</h3><img src='https://i.ibb.co/grH7wvd/marlene.png' alt='Marlène Image'></div>" +
+    "<div>" +
+      "<h3>Adresse</h3>" +
+      "<button id='marlene-button' class='popup-button'><a target=\"_blank\" href=\"https://www.google.com/maps/place/Les+Bavardes/@48.1061729,-1.668653,17z/data=!3m1!4b1!4m6!3m5!1s0x480edf467d5a25d1:0x6c5e499ecdd2ae1a!8m2!3d48.1061729!4d-1.6660781!16s%2Fg%2F11t7cvmf3b?authuser=0&entry=ttu\">Y aller</a></button>" +
+      "<button id='marlene-button' class='popup-button'>Découvrez Son Histoire</button>" +
+    "</div>"
+  );
+
+  L.marker([48.112826336880836, -1.664164031084723], {icon: marie}).addTo(map).bindPopup(
+    "<div><h3>Marie</h3><img src='https://i.ibb.co/9w2PYTB/marie.png' alt='Marie Image'></div>" +
+    "<div>" +
+      "<h3>Adresse</h3>" +
+      "<button id='marie-button' class='popup-button'><a target=\"_blank\" href=\"https://www.google.com/maps/place/Pari-Rennes/@48.1113517,-1.6677568,16.11z/data=!4m6!3m5!1s0x480ede4df99537e9:0x99a93d6dab4a9ce7!8m2!3d48.1125808!4d-1.6642454!16s%2Fg%2F11c5t2yj_6?authuser=0&entry=ttu\">Y aller</a></button>" +
+      "<button id='marie-button' class='popup-button'>Découvrez Son Histoire</button>" +
+    "</div>"
+  );
+
+  L.marker([48.10888969480733, -1.6716351464288817], {icon: frederic}).addTo(map).bindPopup(
+    "<div><h3>Frédéric</h3><img src='https://i.ibb.co/n8FW0K5/frederic.png' alt='Frédéric Image'></div>" +
+    "<div>" +
+      "<h3>Adresse</h3>" +
+      "<button id='frederic-button' class='popup-button'><a target=\"_blank\" href=\"https://www.google.com/maps/place/Moulins+de+Rennes/@48.1086963,-1.6741564,17z/data=!3m1!4b1!4m6!3m5!1s0x480edfb52a6c4a27:0x1c11ba8f1ac65c5a!8m2!3d48.1086963!4d-1.6715815!16s%2Fg%2F1tjdw976?authuser=0&entry=ttu\">Y aller</a></button>" +
+      "<button id='frederic-button' class='popup-button'>Découvrez Son Histoire</button>" +
+    "</div>"
+  );
+
+  L.marker([48.10717800781931, -1.6726050662616248 ], {icon: joachim}).addTo(map).bindPopup(
+    "<div><h3>Joachim</h3><img src='https://i.ibb.co/7KB7V4G/joachim.png' alt='Joachim Image'></div>" +
+    "<div>" +
+      "<h3>Adresse</h3>" +
+      "<button id='joachim-button' class='popup-button'>Y aller</button>" +
+      "<button id='joachim-button' class='popup-button'>Découvrez Son Histoire</button>" +
+    "</div>"
+  );
+
+  L.marker([48.10732876940272, -1.6711969681481214 ], {icon: nicolas}).addTo(map).bindPopup(
+    "<div><h3>Nicolas</h3><img src='https://i.ibb.co/RTFC5SZ/nicolas.png' alt='Nicolas Image'></div>" +
+    "<div>" +
+      "<h3>Adresse</h3>" +
+      "<button id='nicolas-button' class='popup-button'>Y aller</button>" +
+      "<button id='nicolas-button' class='popup-button'>Découvrez Son Histoire</button>" +
+    "</div>"
+  );
+
+  L.marker([48.10855480875185, -1.6518949960950546 ], {icon: christophe}).addTo(map).bindPopup(
+    "<div><h3>Christophe</h3><img src='https://i.ibb.co/zb3pV07/christophe.png' alt='Christophe Image'></div>" +
+    "<div>" +
+      "<h3>Adresse</h3>" +
+      "<button id='christophe-button' class='popup-button'>Y aller</button>" +
+      "<button id='christophe-button' class='popup-button'>Découvrez Son Histoire</button>" +
+    "</div>"
+  );
+}
