6e98b89a4ca26315a730051e1e7118066634722b
leclerc-leo <leoleclercleo@gmail.com>
2024-02-05 00:01:05 +0100

    image pour src choisit si c'est la plus grande, ajout des gif

--- gymnastique-2/image_manager.js
+++ gymnastique-2/image_manager.js
@@ -26,14 +26,21 @@ const iterate = (sharp) => {
                 recursive(path.join(dir, file));
             }
 
-            else {
-                if (!file.endsWith('.jpg') && !file.endsWith('.png')) continue;
+            else if (file.endsWith('.jpg') || file.endsWith('.png')) {
 
                 const input = path.join(dir, file);
                 const file_name = path.basename(file, path.extname(file));
                 const output = path.join(CACHE_PATH, path.relative(IMG_PATH, path.join(dir, file_name)));
 
                 create_versions(sharp, input, output);
+            }
+
+            else if (file.endsWith('.gif')) {
+                const input = path.join(dir, file);
+                const file_name = path.basename(file, path.extname(file));
+                const output = path.join(CACHE_PATH, path.relative(IMG_PATH, path.join(dir, file_name)));
+
+                create_gifs(sharp, input, output);
             }
         }
     }
@@ -71,6 +78,28 @@ const create_versions = (sharp, input, output) => {
     });
 }
 
+const create_gifs = (sharp, input, output) => {
+    [
+        1920,
+        1280,
+        640,
+        320
+    ]
+    .forEach( async width => {
+            
+        const meta = await sharp(input).metadata();
+        width = meta.width > width ? width : meta.width;
+
+        if (!fs.existsSync(output + '-' + width + '.gif')
+            || !fs.lstatSync(input).mtimeMs === fs.lstatSync(output + '-' + width + '.gif').mtimeMs)
+        sharp(input, { animated: true })
+            .resize({ width })
+            .toFile(output + '-' + width + '.gif', (err, info) => {
+                if (err) console.log(err, input, output);
+            });
+    });
+}
+
 const get_elements = (content, regex, elements) => {
     let m;
     while ((m = regex.exec(content)) !== null) {
@@ -81,14 +110,19 @@ const get_elements = (content, regex, elements) => {
 
 const get_width = (file) => {
     const parts = file.split('-');
-    return parts[parts.length - 1].split('.')[0];
+    return parseInt(parts[parts.length - 1].split('.')[0]);
+}
+
+const get_name = (file) => {
+    const parts = file.split('-');
+    return parts.slice(0, parts.length - 1).join('-');
 }
 
 const img_convert = (content, webp_supported) => {
     try {
     let elements = get_elements(content, /<img.*?src=["'](.*?)["']/g, new Set());
 
-    elements = [...elements].filter( file => file.endsWith('.jpg') || file.endsWith('.png'))
+    elements = [...elements].filter( file => file.endsWith('.jpg') || file.endsWith('.png') || file.endsWith('.gif'));
 
     elements = elements.filter( file => file !== 'img/background/frame.jpg');
 
@@ -97,21 +131,29 @@ const img_convert = (content, webp_supported) => {
         let new_file = file.replace('img/', 'img/.cache/')
         const new_file_name = path.basename(new_file, path.extname(new_file));
         new_file = new_file.replace(path.extname(new_file), '');
-        const extension = webp_supported ? '.webp' : '.jpg';
+        const extension = file.endsWith('.gif') ? '.gif' : webp_supported ? '.webp' : '.jpg';
 
         const folder = path.dirname(path.join(PUBLIC_PATH, new_file));
         if (!fs.existsSync(folder)) return;
 
-        const files = fs.readdirSync(folder).filter( f => f.startsWith(new_file_name) && f.endsWith(extension)); 
+        let files = fs.readdirSync(folder).filter( f => get_name(f) === new_file_name && f.endsWith(extension));
         if (files.length === 0) return;
+
+        // get the highest resolution image
+        const index = files.reduce( (acc, f, i) => {
+            const width = get_width(f);
+            return width > get_width(files[acc]) ? i : acc;
+        }, 0);
         
-        const width = get_width(files[0]);
+        const width = get_width(files[index]);
         const new_src = path.join(new_file + '-' + width + extension);
 
-        let srcset = files.map( f => {
+        //remove the index file from the list
+        files.splice(index, 1);
+        let srcset = files.reduce( (acc, f) => {
             const width = get_width(f);
-            return `${path.join(new_file + '-' + width + extension)} ${width}w`;
-        }).join(', ');
+            return acc + `, ${path.join(new_file + '-' + width + extension)} ${width}w`;
+        }, `${new_src} ${width}w`);
 
         srcset = srcset.replace(new_src + ' ' + width + 'w', '');
         srcset = srcset.trim();
