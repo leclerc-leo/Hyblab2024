31efa2667e042eacdc658ba587732ec728ef2400
leclerc-leo <leoleclercleo@gmail.com>
2024-02-04 22:34:57 +0100

    where is the nearest bridge ?

---
+++ gymnastique-2/image_manager.js
@@ -0,0 +1,150 @@
+
+const fs = require('fs'),
+    path = require('path');
+const { exit } = require('process');
+
+const PUBLIC_PATH = path.join(__dirname, 'public');
+const IMG_PATH = path.join(PUBLIC_PATH,'img');
+const CACHE_PATH = path.join(IMG_PATH, '.cache');
+
+const iterate = (sharp) => {
+    /* we want to recreate the folder structure of the img folder into the .cache folder inside the img folder */
+
+    if (!fs.existsSync(CACHE_PATH)) fs.mkdirSync(CACHE_PATH);
+
+    const recursive = (dir) => {
+        for (let file of fs.readdirSync(dir)) {
+            const stat = fs.statSync(path.join(dir, file));
+            
+            if (file.startsWith('.')) continue;
+
+            if (stat.isDirectory()) {
+
+                const new_dir = path.join(CACHE_PATH, path.relative(IMG_PATH, path.join(dir, file)));
+                if (!fs.existsSync(new_dir)) fs.mkdirSync(new_dir);
+
+                recursive(path.join(dir, file));
+            }
+
+            else {
+                if (!file.endsWith('.jpg') && !file.endsWith('.png')) continue;
+
+                const input = path.join(dir, file);
+                const file_name = path.basename(file, path.extname(file));
+                const output = path.join(CACHE_PATH, path.relative(IMG_PATH, path.join(dir, file_name)));
+
+                create_versions(sharp, input, output);
+            }
+        }
+    }
+
+    recursive(IMG_PATH);
+}
+
+const create_versions = (sharp, input, output) => {
+    [
+        1920,
+        1280,
+        640,
+        320
+    ]
+    .forEach( async width => {
+
+        const meta = await sharp(input).metadata();
+        width = meta.width > width ? width : meta.width;
+
+        if (!fs.existsSync(output + '-' + width + '.webp')
+            || !fs.lstatSync(input).mtimeMs === fs.lstatSync(output + '-' + width + '.webp').mtimeMs)
+        sharp(input)
+            .resize({ width })
+            .toFile(output + '-' + width + '.webp', (err, info) => {
+                if (err) console.log(err, input, output + '-' + width + '.webp');
+            });
+
+        if (!fs.existsSync(output + '-' + width + '.jpg')
+            || !fs.lstatSync(input).mtimeMs === fs.lstatSync(output + '-' + width + '.jpg').mtimeMs)
+        sharp(input)
+            .resize({ width })
+            .toFile(output + '-' + width + '.jpg', (err, info) => {
+                if (err) console.log(err, input, output + '-' + width + '.jpg');
+            });
+    });
+}
+
+const get_elements = (content, regex, elements) => {
+    let m;
+    while ((m = regex.exec(content)) !== null) {
+        elements.add(m[1]);
+    }
+    return elements;
+}
+
+const get_width = (file) => {
+    const parts = file.split('-');
+    return parts[parts.length - 1].split('.')[0];
+}
+
+const img_convert = (content, webp_supported) => {
+    try {
+    let elements = get_elements(content, /<img.*?src=["'](.*?)["']/g, new Set());
+
+    elements = [...elements].filter( file => file.endsWith('.jpg') || file.endsWith('.png'))
+
+    console.log(elements);
+
+    elements.forEach( file => {
+
+        let new_file = file.replace('img/', 'img/.cache/')
+        const new_file_name = path.basename(new_file, path.extname(new_file));
+        new_file = new_file.replace(path.extname(new_file), '');
+        const extension = webp_supported ? '.webp' : '.jpg';
+
+        // replace the src attributes with the biggest image available
+        // and add the srcset attribute with all the other images
+
+        const folder = path.dirname(path.join(PUBLIC_PATH, new_file));
+        const files = fs.readdirSync(folder).filter( f => f.startsWith(new_file_name) && f.endsWith(extension)); 
+        
+        const width = get_width(files[0]);
+        const new_src = path.join(new_file + '-' + width + extension);
+
+        let srcset = files.map( f => {
+            const width = get_width(f);
+            return `${path.join(new_file + '-' + width + extension)} ${width}w`;
+        }).join(', ');
+
+        srcset = srcset.replace(new_src + ' ' + width + 'w', '');
+        srcset = srcset.trim();
+
+        content = content.replaceAll(file, `${new_src}" srcset="${srcset}`);
+    });
+
+    //return content;
+
+    } 
+    catch (e) {
+        console.log(e);
+        return null;
+    }
+}
+
+const make = () => {
+    try {
+        const sharp = require('sharp');
+
+        try {
+        iterate(sharp);
+
+        }
+        catch (e) {
+            console.log(e);
+            return;
+        }
+    } 
+    catch (e) {
+        console.log('sharp is not installed : Not converting images to webp and resizing images');
+        return;
+    }
+}
+
+module.exports = { make, img_convert };
--- gymnastique-2/server.js
+++ gymnastique-2/server.js
@@ -2,7 +2,8 @@
 
 const express = require('express'), 
     path = require('path'), 
-    fs = require('fs');
+    fs = require('fs'),
+    { make, img_convert } = require('./image_manager');
 
 const app = express();
 
@@ -19,34 +20,6 @@ app.use((req, _, next) => {
     next();
 });
 
-const get_files = (content, regex, files) => {
-    let m;
-    while ((m = regex.exec(content)) !== null) {
-        files.add(m[1]);
-    }
-    return files;
-}
-
-const convert_to_webp = (content) => {
-    let files = get_files(content, /<img.*?src=["'](.*?)["']/g, new Set());
-    files = get_files(content, /background-image: url\(["'](.*?)["']\)/g, files);
-
-    files = [...files].filter( file => file.endsWith('.jpg') || file.endsWith('.png'))
-        .filter( file => { /* On vérifie que le fichier webp correspondant existe */
-            const f = file.split('/').slice(-1)[0];
-            return fs.existsSync(path.join(path.join(public_path, 'img', 'webp'), path.basename(f, path.extname(f)) + '.webp'));
-        });
-
-    files.forEach( file => {
-        const f = file.split('/').slice(-1)[0];
-        const file_name = path.basename(f, path.extname(f));
-        
-        content = content.replace(new RegExp(file, 'g'), 'img/webp/' + file_name + '.webp');
-    });
-
-    return content;
-}
-
 app.get('/', function(req, res) {
         const files = fs.readdirSync(views_path).map( file => {
         return [file.split('.')[0], fs.readFileSync(path.join(views_path, file), 'utf8')];
@@ -58,9 +31,15 @@ app.get('/', function(req, res) {
         return acc.replace(`<%= ${file[0]} %>`, file[1]);
     }, layout);    
 
-    if (req.webp_supported) combined = convert_to_webp(combined);
+    let temp = combined;
+    combined = img_convert(combined, req.webp_supported);
+    if (combined === undefined || combined === null) {
+        combined = temp;
+    }
 
     res.send(combined);
 });
+
+make();
 
 module.exports = app;
