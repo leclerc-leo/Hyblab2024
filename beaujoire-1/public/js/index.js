035201a4fcc75033fbdff0734517d6965cad9354
Thibaut Sophys <tsophys@gmail.com>
2024-02-05 00:34:09 +0100

    Added intro and adjusted first page

--- beaujoire-1/public/js/index.js
+++ beaujoire-1/public/js/index.js
@@ -1,1 +1,14 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+	document.getElementById("intro-btn").addEventListener("click", function () {
+		document.getElementById("intro").style.display = "none";
+		let gif = document.getElementById("gif").children[0];
+		gif.src = "./img/logo.gif";
+		gif.style.display = "flex";
+
+		setTimeout(function () {
+			document.getElementById("gif").style.display = "none";
+		}, 6040);
+	});
+});
