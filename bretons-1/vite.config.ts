3b022de75e45d7c04752b5ab723243bcfd373079
Nino <nino.auriede@etu.univ-nantes.fr>
2024-02-06 09:52:20 +0100

    saucisson

--- bretons-1/vite.config.ts
+++ bretons-1/vite.config.ts
@@ -1,23 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: './public',
-    rollupOptions: {
-      external: ["react", "react-router", "react-router-dom", "react-redux"],
-      output: {
-        globals: {
-          react: "React",
-          "react-router": "ReactRouter",
-          "react-router-dom": "ReactRouterDOM",
-          "react-redux": "ReactRedux",
-        },
-      },
-    },
   },
-  publicDir: './src/assets',
-  base: '/bretons-1/', // Add a leading slash to fix the base path
-});
+  publicDir:'./src/assets',
+  base:"bretons-1"
+})
