3a1c1e1b5b09892c79d409cf3d35d5d76744f8c5
Nino <nino.auriede@etu.univ-nantes.fr>
2024-02-06 09:38:23 +0100

    test page mysave et crédits

--- bretons-1/vite.config.ts
+++ bretons-1/vite.config.ts
@@ -6,7 +6,16 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: './public',
+    rollupOptions: {
+      external: ["react", "react-router", "react-router-dom", "react-redux"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      }
+    }
   },
   publicDir:'./src/assets',
-  base:"bretons-1"
+  base:"bretons-1",
+  
 })
