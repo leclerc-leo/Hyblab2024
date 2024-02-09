aec2e0dfb621f601b8a5c67edb341c5657c464b5
E203280G <octave.daudin@etu.univ-nantes.fr>
2024-02-09 01:18:53 +0100

    update mouvement

--- gymnastique-2/public/js/slides/mouvement.js
+++ gymnastique-2/public/js/slides/mouvement.js
@@ -1,33 +1,27 @@
-const pageMouvement = document.querySelector("#mouvement-container");
-const boutonG = pageMouvement.querySelector("#mouvement-contenue-titre-buttonG");
-const boutonD = pageMouvement.querySelector("#mouvement-contenue-titre-buttonD");
-const titre = pageMouvement.querySelector("#mouvement-contenue-titre-texte");
-const texte = pageMouvement.querySelector("#mouvement-contenue-texte");
-function getActif(element) {
-    const id = element.querySelector(".mouvActif").id.split("-");
-    return parseInt(id[id.length-1]);
-}
-boutonG.addEventListener("click",()=>{
-    const idActif = getActif(titre);
-    boutonD.classList.remove("hidden");
-    titre.querySelector(("#mouvement-contenue-titre-texte-"+idActif)).classList.replace("mouvActif","hidden");
-    titre.querySelector(("#mouvement-contenue-titre-texte-"+(idActif-1))).classList.replace("hidden","mouvActif");
-    texte.querySelector(("#mouvement-contenue-texte-"+idActif)).classList.replace("mouvActif","hidden");
-    texte.querySelector(("#mouvement-contenue-texte-"+(idActif-1))).classList.replace("hidden","mouvActif");
-    console.log(idActif);
-    if(idActif-1===1){
-        boutonG.classList.add("hidden");
+const swiperMouvement = new Swiper('#swiper_mouvement', {
+    direction: "horizontal",
+    loop: false,
+    mousewheel: true,
+    navigation: {
+        enabled: false,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     }
+
 });
-boutonD.addEventListener("click",()=>{
-    const idActif = getActif(titre);
-    boutonG.classList.remove("hidden");
-    console.log(idActif);
-    titre.querySelector(("#mouvement-contenue-titre-texte-"+idActif)).classList.replace("mouvActif","hidden");
-    titre.querySelector(("#mouvement-contenue-titre-texte-"+(idActif+1))).classList.replace("hidden","mouvActif");
-    texte.querySelector(("#mouvement-contenue-texte-"+idActif)).classList.replace("mouvActif","hidden");
-    texte.querySelector(("#mouvement-contenue-texte-"+(idActif+1))).classList.replace("hidden","mouvActif");
-    if(idActif+1===titre.childElementCount){
-        boutonD.classList.add("hidden");
-    }
+
+const fleche_gauche_mouv = document.querySelectorAll('#fleche_gauche_mouv');
+fleche_gauche_mouv.forEach( fleche => {
+    fleche.addEventListener('click', () => {
+        swiperMouvement.slidePrev();
+        console.log("ouiG")
+    });
+});
+
+const fleche_droite_mouv = document.querySelectorAll('#fleche_droite_mouv');
+fleche_droite_mouv.forEach( fleche => {
+    fleche.addEventListener('click', () => {
+        console.log("ouiD")
+        swiperMouvement.slideNext();
+    });
 });
