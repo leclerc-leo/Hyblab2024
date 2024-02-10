206930565e4925c7037730e1c7193c223004c39a
E203280G <octave.daudin@etu.univ-nantes.fr>
2024-02-10 01:58:07 +0100

    PAS FINI ! (juste du placement à faire et une animation pour la taille) changement page palmares

--- gymnastique-2/public/js/slides/palmares.js
+++ gymnastique-2/public/js/slides/palmares.js
@@ -16,24 +16,35 @@ allcard.forEach(card_container =>{
         });
         promise.then(()=>{
             allcard.forEach(async (card) => {
+                if(card_container.id == "card-mondiaux"){
+                    if(card_container.children[1].classList.contains("flipcard")){
+                        card_container.style.transform = "translate(0,-8vh)";
+                    }
+                    else{
+                        card_container.style.transform = "translate(0,0)";
+                    }
+                }
             if(card != card_container){
                 card.children[1].classList.remove("flipcard");
+                const img_gif = card.children[0];
+                img_gif.classList.contains("hidden") ?
+                    setTimeout(()=>{img_gif.classList.toggle("hidden");},1000) : img_gif.classList.toggle("hidden");
                 if(card_container.children[1].classList.contains("flipcard")){
                     //card.style.display ="none";
                     card.classList.add("smaller");
-                    if(card.id==="card-mondiaux" || card.id==="card-inter"){
+                    if(card.id==="card-mondiaux"){
                         card_container.id==="card-afrique" || card_container.id==="card-inter" ?
-                        card.style.transform = "translate(0px,10vh)" :
+                        card.style.transform = "translate(0px,15vh)" :
                             card.style.transform = "translate(0,0)";
                     }
-                    /*
+
                     if(card.id==="card-inter"){
                         if(card_container.id==="card-afrique") {
-                            card.style.transform = "translate(0,15vh) scale(1,0.5)"
+                            card.style.transform = "translate(0,15vh)"
                         }else{
-                            card.style.transform = "translate(0,0) scale(1,0.5)";
+                            card.style.transform = "translate(0,0)";
                         }
-                    }*/
+                    }
 
                 }else{
                     //card.style.display ="";
