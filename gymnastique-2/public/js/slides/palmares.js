da5bce45d80afc4e86c98650ba4d94da8be23799
E203280G <octave.daudin@etu.univ-nantes.fr>
2024-02-10 01:43:58 +0100

    PAS FINI ! changement page palmares

--- gymnastique-2/public/js/slides/palmares.js
+++ gymnastique-2/public/js/slides/palmares.js
@@ -1,12 +1,49 @@
-document.querySelectorAll('.card-container').forEach(card_container =>{
+const allcard = document.querySelectorAll('.card-container');
+allcard.forEach(card_container =>{
     card_container.addEventListener("click",()=>{
-        if(card_container.childElementCount>1){
-            card_container.children[1].classList.toggle("flipcard");
-            const img_gif = card_container.children[0];
-            img_gif.classList.contains("hidden") ?
-                setTimeout(()=>{img_gif.classList.toggle("hidden");},1000) : img_gif.classList.toggle("hidden");
-        }else {
-            card_container.children[0].classList.toggle("flipcard");
-        }
+        const promise = new  Promise((resolve,reject)=>{
+            card_container.classList.remove("smaller");
+            card_container.style.transform = "translate(0,0)";
+            if(card_container.childElementCount>1){
+                card_container.children[1].classList.toggle("flipcard");
+                const img_gif = card_container.children[0];
+                img_gif.classList.contains("hidden") ?
+                    setTimeout(()=>{img_gif.classList.toggle("hidden");},1000) : img_gif.classList.toggle("hidden");
+            }else {
+                card_container.children[0].classList.toggle("flipcard");
+            }
+            resolve("ok")
+        });
+        promise.then(()=>{
+            allcard.forEach(async (card) => {
+            if(card != card_container){
+                card.children[1].classList.remove("flipcard");
+                if(card_container.children[1].classList.contains("flipcard")){
+                    //card.style.display ="none";
+                    card.classList.add("smaller");
+                    if(card.id==="card-mondiaux" || card.id==="card-inter"){
+                        card_container.id==="card-afrique" || card_container.id==="card-inter" ?
+                        card.style.transform = "translate(0px,10vh)" :
+                            card.style.transform = "translate(0,0)";
+                    }
+                    /*
+                    if(card.id==="card-inter"){
+                        if(card_container.id==="card-afrique") {
+                            card.style.transform = "translate(0,15vh) scale(1,0.5)"
+                        }else{
+                            card.style.transform = "translate(0,0) scale(1,0.5)";
+                        }
+                    }*/
+
+                }else{
+                    //card.style.display ="";
+                    card.classList.remove("smaller");
+                    card.style.transform = "translate(0,0)";
+
+
+                }
+            }
+        })});
     });
+
 });
