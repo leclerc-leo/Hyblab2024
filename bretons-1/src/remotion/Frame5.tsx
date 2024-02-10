0462181257b73e1c637682854eb46c3173af669b
Nino <nino.auriede@etu.univ-nantes.fr>
2024-02-10 18:37:19 +0100

    changement logo dasn

--- bretons-1/src/remotion/Frame5.tsx
+++ bretons-1/src/remotion/Frame5.tsx
@@ -21,16 +21,6 @@ const pointeurImagePositions: { [key: string]: pointeurPositions } = {
   "Quimper": { top: "86%", left: "23%" }
 };
 
-const getImage = (sport: string) => {
-  switch (sport) {
-    case "Cyclisme":
-      return '/bretons-1/img/velo.svg'
-    case "Voile":
-      return '/bretons-1/img/Voile.svg'
-    default:
-      return ""
-  }
-}
 
 export const Frame5: React.FC<{ nom: string, age: string, sexe: string, sport: string, fun_fact: string, photo: string, record: string, ville: string }> = ({ nom, age, sexe, sport, fun_fact, photo, record, ville }) => {
 
@@ -56,7 +46,7 @@ export const Frame5: React.FC<{ nom: string, age: string, sexe: string, sport: s
         <div className="age-frame5">{age}</div>
         <div className="taille-frame5">{sexe}</div>
         <div className="sport-frame5">{sport}</div>
-        <img src={getImage(sport)} className="sport-image"></img>
+        <img src={`/bretons-1/img/${sport}.svg`} className="sport-image"></img>
       </div>
       {nom === "Marie Bolou" ? (
         <>
