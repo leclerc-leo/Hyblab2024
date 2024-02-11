598073c11886e07f9494e8f52b9c76eb165f747c
Arthur <arthur.courtrai@etu.univ-nantes.fr>
2024-02-11 10:23:54 +0100

    Changeme css bug + modif tsx

--- bretons-1/src/remotion/Frame2.tsx
+++ bretons-1/src/remotion/Frame2.tsx
@@ -94,7 +94,7 @@ import { Podium } from '../components/type';
           const flagFrameClass = index === 0 ? "flagFrame2" : index === 1 ? "flag_2_Frame2" : "flag_3_Frame2";
           const nameFrameClass = index === 0 ? "nameFrame2" : index === 1 ? "name_2_Frame2" : "name_3_Frame2";
           const timeFrameClass = index === 0 ? "timeFrame2" : index === 1 ? "time_2_Frame2" : "time_3_Frame2";
-          const ContainerClass = index === 0 ? "containerFrame2" : "";
+          const ContainerClass = index === 0 || index === 1 ? "containerFrame2" : "";
           const countryFrameClass = index === 0 ? "countryFrame2" : index === 1 ? "country_2_Frame2" : "country_3_Frame2";
           const highlightClass = athlete === entry.Nom ? "highlight_" : "";
           const colorClass = athlete === entry.Nom ? "color_" : "";
