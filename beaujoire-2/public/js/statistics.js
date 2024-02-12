5dd92849834cf2c7e2b5a94dd0d4dee76c1e5e92
HadilHelali <helalihadil4@gmail.com>
2024-02-12 15:17:13 +0100

    [FIX] Fixing the players positions style

--- beaujoire-2/public/js/statistics.js
+++ beaujoire-2/public/js/statistics.js
@@ -5,6 +5,21 @@ function isInPopularTeam(playerId) {
     return popularTeam.some(player => player.id === playerId);
 }
 
+// Wait for the content to preload and display 1st slide
+// Here we simulate a loading time of one second
+setTimeout(() => {
+    // fade out the loader "slide"
+    // and send it to the back (z-index = -1)
+    anime({
+        delay: 100,
+        targets: '#loader',
+        opacity: '0',
+        'z-index' : -1,
+        easing: 'easeOutQuad',
+    });
+}, 400);
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const teams = ['Votre équipe', 'Équipe Majoritaire'];
 
