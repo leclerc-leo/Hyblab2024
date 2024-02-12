fd0f688bfd19effd041c4724910081e71e958e1d
Souha <souha.loulou@insat.ucar.tn>
2024-02-12 16:56:53 +0100

    [EVO] creating the individual cards of the players and linking them to the database

--- beaujoire-2/public/js/list.js
+++ beaujoire-2/public/js/list.js
@@ -127,130 +127,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   }
 
-  function downSlide(button, playerBox){
-    if (button.dataset.type === 'downslide') {
-      const player = JSON.parse(playerBox.dataset.player);
-      console.log(player);
-      const imagePlayer = playerBox.children[0];
-      const name = playerBox.children[1].children[0];
-      const heart = playerBox.children[2];
-      const slider = playerBox.children[1].children[1];
-    
-      imagePlayer.style["display"] = "none";
-      name.style["display"] = "none";
-      heart.style["display"] = "none";
-      slider.style["display"] = "none";
-
-      try {
-        //const detailedPlayer =  globals.getPlayersById(playerId);
-        const backgroundText = document.createElement("div");
-        backgroundText.className = "background-text";
-        backgroundText.textContent = "FC Nantes  FC Nantes  FC Nantes  FC Nantes";
-
-        const avatar = document.createElement("img");
-        avatar.src = player.photo;
-        avatar.alt = "Player Image";
-        avatar.className = "avatar";
-
-        const overlayFirstname = document.createElement("div");
-        overlayFirstname.className = "overlay-firstname";
-        overlayFirstname.textContent = `${player.prenom}`;
-
-        const lastname = document.createElement("div");
-        lastname.className = "lastname";
-        lastname.textContent = `${player.nom}`;
-
-        const fullname = document.createElement("div");
-        fullname.className = "fullname";
-        fullname.appendChild(overlayFirstname);
-        fullname.appendChild(lastname);
-
-        const closeButton = document.createElement("img");
-        closeButton.src = "./img/field/croix.svg";
-        const displayBio = document.createElement("div");
-        displayBio.className = "display-bio";
-        displayBio.appendChild(closeButton);
-
-        const field = document.createElement("img");
-        field.src = "./img/bio-icons/selection.svg";
-        const nbMatchs = document.createElement("p");
-        nbMatchs.textContent = `${player.selections} séléctions`;
-        const infos1 = document.createElement("div");
-        infos1.className = "infos";
-        infos1.appendChild(field);
-        infos1.appendChild(nbMatchs);
-
-        const flag1 = document.createElement("img");
-        flag1.src = `./img/nationalities/${player.nationalité1}.svg`;
-        const flag2 = document.createElement("img");
-        flag2.src = `./img/nationalities/${player.nationalité2}.svg`;
-        const flags = document.createElement("div");
-        flags.className = "flags";
-        flags.appendChild(flag1);
-        flags.appendChild(flag2);
-        const countries = document.createElement("p");
-        countries.textContent = "PAYS1/PAYS2"
-        const infos2 = document.createElement("div");
-        infos2.className = "infos";
-        infos2.appendChild(flags);
-        infos2.appendChild(countries);
-
-        const likeButton = document.createElement("img");
-        likeButton.src = "./img/COEUR1.svg";
-        const like = document.createElement("div");
-        like.className = "like";
-        like.appendChild(likeButton);
-
-        const logoFCN = document.createElement("img");
-        logoFCN.src = "./img/bio-icons/club-icon/FCNANTES-2019.svg";
-        const yearInClub = document.createElement("p");
-        yearInClub.textContent = "2024";
-        const infos3 = document.createElement("div");
-        infos3.className = "infos";
-        infos3.appendChild(logoFCN);
-        infos3.appendChild(yearInClub);
-        let role = document.createElement("img");
-        let score = document.createElement("p");
-        if (player.poste === 1){
-            role.src = "./img/bio-icons/arret.svg";
-            score.textContent = `${player.arrets} arrêts`;
-        }
-        else if (player.poste < 12){
-            role.src = "./img/bio-icons/ballon.svg";
-            score.textContent = `${player.buts} buts`;
-        }
-        const infos4 = document.createElement("div");
-        infos4.className = "infos";
-        infos4.appendChild(role);
-        infos4.appendChild(score);
-        const icons = document.createElement("div");
-        icons.className = "icons";
-        icons.appendChild(infos1);
-        icons.appendChild(infos2);
-        icons.appendChild(like);
-        icons.appendChild(infos3);
-        icons.appendChild(infos4);
-
-        const iconsText = document.createElement("div");
-        iconsText.className = "icons-text";
-        iconsText.textContent = `${player.biographie}`
-        
-        playerBox.className = "bio-player";
-        playerBox.appendChild(backgroundText);
-        playerBox.appendChild(avatar);
-        playerBox.appendChild(fullname);
-        playerBox.appendChild(displayBio);
-        playerBox.appendChild(icons);
-        playerBox.appendChild(iconsText);
-
-        likeButton.addEventListener('click', () => { toggleHeart(likeButton, player.id, positionId); });
-      } catch (error) {
-        console.error(error);
-      } 
-    }
-  };
-
-
   function toggleVote(positionId,playerId){
     if (globals.tabVotes[positionId-1] === 0)
     {
@@ -277,6 +153,138 @@ document.addEventListener('DOMContentLoaded', function () {
       selectedHeartButton = button;
     }
   }
+
+  function downSlide(button, playerBox){
+    if (button.dataset.type === 'downslide') {
+      const player = JSON.parse(playerBox.dataset.player);
+      console.log(player);
+      const imagePlayer = playerBox.children[0];
+      const name = playerBox.children[1].children[0];
+      const heart = playerBox.children[2];
+      const slider = playerBox.children[1].children[1];
+    
+      imagePlayer.style["display"] = "none";
+      name.style["display"] = "none";
+      heart.style["display"] = "none";
+      slider.style["display"] = "none";
+      playerBox.classList.toggle('hidden-content');
+      try {
+        //const detailedPlayer =  globals.getPlayersById(playerId);
+        const backgroundText = document.createElement("div");
+        backgroundText.className = "background-text";
+        backgroundText.textContent = "FC Nantes  FC Nantes  FC Nantes  FC Nantes";
+
+        const avatar = document.createElement("img");
+        avatar.src = player.photo;
+        avatar.alt = "Player Image";
+        avatar.className = "avatar";
+
+        const overlayFirstname = document.createElement("div");
+        overlayFirstname.className = "overlay-firstname";
+        overlayFirstname.textContent = `${player.prenom}`;
+
+        const lastname = document.createElement("div");
+        lastname.className = "lastname";
+        lastname.textContent = `${player.nom}`;
+
+        const fullname = document.createElement("div");
+        fullname.className = "fullname";
+        fullname.appendChild(overlayFirstname);
+        fullname.appendChild(lastname);
+
+        const closeButton = document.createElement("img");
+        closeButton.src = "./img/field/croix.svg";
+        closeButton.addEventListener('click', () => { closeBio(playerBox); });
+        const displayBio = document.createElement("div");
+        displayBio.className = "display-bio";
+        displayBio.appendChild(closeButton);
+
+        const field = document.createElement("img");
+        field.src = "./img/bio-icons/selection.svg";
+        const nbMatchs = document.createElement("p");
+        nbMatchs.textContent = `${player.selections} séléctions`;
+        const infos1 = document.createElement("div");
+        infos1.className = "infos";
+        infos1.appendChild(field);
+        infos1.appendChild(nbMatchs);
+
+        const flag1 = document.createElement("img");
+        flag1.src = `./img/nationalities/${player.nationalité1}.svg`;
+        const flag2 = document.createElement("img");
+        let c2;
+        if (player.nationalité2 != null){
+        flag2.src = `./img/nationalities/${player.nationalité2}.svg`;
+        c2= "/PAYS2"
+        }
+        else{c2="";}
+        const flags = document.createElement("div");
+        flags.className = "flags";
+        flags.appendChild(flag1);
+        flags.appendChild(flag2);
+        const countries = document.createElement("p");
+        countries.textContent = `PAYS1${c2}`
+        const infos2 = document.createElement("div");
+        infos2.className = "infos";
+        infos2.appendChild(flags);
+        infos2.appendChild(countries);
+
+        const logoFCN = document.createElement("img");
+        logoFCN.src = "./img/bio-icons/club-icon/FCNANTES-2019.svg";
+        const yearInClub = document.createElement("p");
+        if (player.annéeFin != null){
+        yearInClub.textContent = `${player.annéeDébut} - ${player.annéeFin}`;}
+        else {yearInClub.textContent = `${player.annéeDébut} - Présent`}
+        const infos3 = document.createElement("div");
+        infos3.className = "infos";
+        infos3.appendChild(logoFCN);
+        infos3.appendChild(yearInClub);
+        let role = document.createElement("img");
+        let score = document.createElement("p");
+        if (player.poste === 1){
+            role.src = "./img/bio-icons/arret.svg";
+            score.textContent = `${player.arrets} arrêts`;
+        }
+        else if (player.poste < 12){
+            role.src = "./img/bio-icons/ballon.svg";
+            score.textContent = `${player.buts} buts`;
+        }
+        const infos4 = document.createElement("div");
+        infos4.className = "infos";
+        infos4.appendChild(role);
+        infos4.appendChild(score);
+        const icons = document.createElement("div");
+        icons.className = "icons";
+        icons.appendChild(infos1);
+        icons.appendChild(infos2);
+        icons.appendChild(infos3);
+        icons.appendChild(infos4);
+
+        const iconsText = document.createElement("div");
+        iconsText.className = "icons-text";
+        iconsText.textContent = `${player.biographie}`
+        
+        playerBox.className = "bio-player";
+        playerBox.appendChild(backgroundText);
+        playerBox.appendChild(avatar);
+        playerBox.appendChild(fullname);
+        playerBox.appendChild(displayBio);
+        playerBox.appendChild(icons);
+        playerBox.appendChild(iconsText);
+
+
+      } catch (error) {
+        console.error(error);
+      } 
+    }
+
+    function closeBio(playerBox) {
+        playerSwiper.slideTo(playerSwiper.realIndex);
+        const yOffset = playerBox.offsetTop - playerSwiper.wrapperEl.offsetTop;
+        playerSwiper.wrapperEl.scrollTop = yOffset;
+    }
+
+  };
+
 
 
   /*function updateContent(currentIndex) {
