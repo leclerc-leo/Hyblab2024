308ffa48913c56eaab61cf0dc21334176f9ece39
Thibaut Sophys <tsophys@gmail.com>
2024-02-12 11:51:19 +0100

    some slight update to css to avoid blue highlight

--- beaujoire-1/public/js/selection.js
+++ beaujoire-1/public/js/selection.js
@@ -136,46 +136,45 @@ document.addEventListener("DOMContentLoaded", function () {
 		console.log("Ranked compositions:", sortedButtons);
 	}
 
-  
-  heartButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-      const heartOutline = this.querySelector('.heart-outline');
-      const heartFilled = this.querySelector('.heart-filled');
-      const isLiked = heartFilled.style.display === 'block';
-      
-      if (isLiked) {
-        // Logique pour retirer le like
-        heartOutline.style.display = 'block';
-        heartFilled.style.display = 'none';
-        updateLikes(this, -1);
-      } else {
-        // Logique pour ajouter un like
-        heartOutline.style.display = 'none';
-        heartFilled.style.display = 'block';
-        updateLikes(this, 1);
-      }
-    });
-  });
+	heartButtons.forEach(function (button) {
+		button.addEventListener("click", function () {
+			const heartOutline = this.querySelector(".heart-outline");
+			const heartFilled = this.querySelector(".heart-filled");
+			const isLiked = heartFilled.style.display === "block";
 
-  function updateLikes(button, delta) {
-	const likeCountSpan = button.querySelector('.like-count');
-	let likes = parseInt(button.dataset.likes, 10) || 0;
-	likes += delta;
-	button.dataset.likes = likes; // Met à jour le nombre de likes dans l'attribut data-likes
-	likeCountSpan.textContent = likes; // Affiche le nombre mis à jour
-  
-	// Mise à jour du localStorage pour persister l'état liké
-	const compositionId = button.getAttribute('data-composition-id');
-	if (delta > 0) {
-	  localStorage.setItem(compositionId, likes.toString()); // Stocke le nombre actuel de likes
-	} else {
-	  const storedLikes = parseInt(localStorage.getItem(compositionId), 10);
-	  if (storedLikes <= likes) {
-		localStorage.setItem(compositionId, likes.toString()); // Stocke le nombre actuel de likes
-	  }
+			if (isLiked) {
+				// Logique pour retirer le like
+				heartOutline.style.display = "block";
+				heartFilled.style.display = "none";
+				updateLikes(this, -1);
+			} else {
+				// Logique pour ajouter un like
+				heartOutline.style.display = "none";
+				heartFilled.style.display = "block";
+				updateLikes(this, 1);
+			}
+		});
+	});
+
+	function updateLikes(button, delta) {
+		const likeCountSpan = button.querySelector(".like-count");
+		let likes = parseInt(button.dataset.likes, 10) || 0;
+		likes += delta;
+		button.dataset.likes = likes; // Met à jour le nombre de likes dans l'attribut data-likes
+		likeCountSpan.textContent = likes; // Affiche le nombre mis à jour
+
+		// Mise à jour du localStorage pour persister l'état liké
+		const compositionId = button.getAttribute("data-composition-id");
+		if (delta > 0) {
+			localStorage.setItem(compositionId, likes.toString()); // Stocke le nombre actuel de likes
+		} else {
+			const storedLikes = parseInt(
+				localStorage.getItem(compositionId),
+				10
+			);
+			if (storedLikes <= likes) {
+				localStorage.setItem(compositionId, likes.toString()); // Stocke le nombre actuel de likes
+			}
+		}
 	}
-  }
-  
-
-  
 });
