8c73a4487b591e7df403441e47e3c922f06f76d8
HadilHelali <helalihadil4@gmail.com>
2024-02-12 16:44:51 +0100

    [FIX] Fixing the vote progress mecanism

--- beaujoire-2/public/js/field.js
+++ beaujoire-2/public/js/field.js
@@ -2,6 +2,22 @@
 console.log(globals.tabVotes);
 console.log(globals.shown);
 
+// Wait for the content to preload and display 1st slide
+// Here we simulate a loading time of one second
+setTimeout(() => {
+  // fade out the loader "slide"
+  // and send it to the back (z-index = -1)
+  anime({
+    delay: 100,
+    targets: '#loader',
+    opacity: '0',
+    'z-index' : -1,
+    easing: 'easeOutQuad',
+  });
+  // Init first slide
+}, 700);
+
+
 /************* Archives *************/
 function checkVotes() {
   const archivesButton = document.getElementById('archives');
